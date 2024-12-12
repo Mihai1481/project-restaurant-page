09bfca7e3a3ccf5d1880761c52ddc51d21740072
Mihai1481 <mihaieduard1481@gmail.com>
2024-12-12 17:38:57 +0200

    Add header and footer creation functions to index.js and update template.html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+function createTitle() {
+  const header = document.querySelector("header");
+  header.classList.add("header");
+
+  const title = document.createElement("h1");
+  title.textContent = "Sport Pub";
+
+  header.insertBefore(title, header.firstChild);
+
+  return header;
+}
+
+function createFooter() {
+  const footer = document.querySelector("footer");
+
+  footer.textContent = "Copyright © 2024 Mihai1481";
+
+  return footer;
+}
+
+createTitle();
+createFooter();
